920d596ad9c9a3515e5b183360024b62d4a1a237
kulterryan <chaurasia.aryan@yahoo.com>
2021-11-28 01:03:58 +0530

    Final Checkpoint

--- Project OOP/program/src/tech/aryn/helloworld/Main.java
+++ Project OOP/program/src/tech/aryn/helloworld/Main.java
@@ -1,40 +1,17 @@
 package tech.aryn.helloworld;
 
+import java.util.*;
 import java.util.Scanner;
-
-class Address{
-    private String street;
-    private String city;
-    private String state;
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public void getAddress(){
-        System.out.println("Address of Passenger: ");
-        System.out.println("Street: " + street);
-        System.out.println("City: " + city);
-        System.out.println("State: " + state);
-    }
-}
 
 class Flight{
 
     // Init
     private int flightNumber = 123;
-    String airline = "Air India";
-    int capacity = 220 ;
-    int seats_booked = 0;
-    int seats_avail;
+    private String airline = "Air India";
+    private int capacity = 220 ;
+    private int seats_booked = 0;
+    private int seats_avail;
+    private int newPNR;
 
     // Constructor - Flight
     void flight(int flightNumber, String airline, int capacity, int seats_booked, int seats_avail){
@@ -43,6 +20,24 @@ class Flight{
         this.capacity = capacity;
         this.seats_booked = seats_booked;
         this.seats_avail = seats_avail;
+    }
+
+    private static HashMap<Integer, Integer> pnrMap;
+    private static HashMap<Integer, String> nameMap;
+    private static HashMap<Integer, String> Location;
+
+    public Flight(){
+        nameMap = new HashMap();
+        Location = new HashMap();
+        pnrMap = new HashMap();
+    }
+
+    public HashMap<Integer, Integer> getPnrMap(){
+        return pnrMap;
+    }
+
+    public HashMap<Integer, String> getLocation(){
+        return Location;
     }
 
     // Output
@@ -54,13 +49,16 @@ class Flight{
         System.out.println("Seats Booked: " + this.seats_booked);
         System.out.println("Seats Available: " + this.seats_avail);
     }
+
+    public void newPNR(int pnr){
+        this.newPNR = pnr;
+    }
 }
 
 class Passenger{
 
-    private int passengerID=0;
+    private static int idCounter=0;
     Scanner sc = new Scanner(System.in);
-    private int idCounter;
 
     Passenger(){
         this.idCounter = this.idCounter + 1;
@@ -69,42 +67,31 @@ class Passenger{
     public int getPassengerCount(){
         return this.idCounter;
     }
-}
 
-class Contact {
-    String name;
-    String email;
-    String phone;
+    public void ContactObj(int phone, String name, String email){
+        Contact contact = new Contact();
 
-    public void getContact(String name, String phone, String email){
-        System.out.println("Contact Details of Passenger: ");
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
+        System.out.println("Please Enter Contact Details: ");
+        System.out.println("Name: ");
+        name = sc.nextLine();
+        System.out.println("Phone: ");
+        phone = sc.nextInt();
+        System.out.println("Email: ");
+        email = sc.nextLine();
 
-        System.out.println("Name: " + this.name);
-        System.out.println("Phone: " + this.phone);
-        System.out.println("Email: " + this.email);
-    }
-
-    // Set Attributes
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+        contact.setName(name);
+        contact.setPhone(phone);
+        contact.setEmail(email);
+        contact.getContact(name, phone, email);
     }
 }
+
 
 
 // Ticket Class
 abstract class Ticket {
 
-    private String ticketPNR;
+    private int ticketPNR;
     private String ticketClass;
     private String ticketPrice;
     private String destination;
@@ -112,7 +99,7 @@ abstract class Ticket {
     private int seatNum;
     private boolean ticketStat;
 
-    public Ticket(String ticketPNR, String ticketClass, String ticketPrice, String destination, int departure, int seatNum, boolean ticketStat) {
+    public Ticket(int ticketPNR, String ticketClass, String ticketPrice, String destination, int departure, int seatNum, boolean ticketStat) {
         //Constructor
         this.ticketPNR = ticketPNR;
         this.ticketClass = ticketClass;
@@ -140,7 +127,7 @@ abstract class Ticket {
     }
 
     public int getPnr(){
-        return ticketPNR.hashCode();
+        return ticketPNR;
     }
 
     public void setTicketStat(boolean ticketStat) {
@@ -154,56 +141,60 @@ abstract class Ticket {
 }
 
 
-// Inherting Ticket Class in Regular Tickets
+// Inheriting Ticket Class in Regular Tickets
 class RegularTicket extends Ticket{
 
-    boolean snacks;
-    boolean food;
-    boolean water;
+    private String specialService;
 
-    RegularTicket(String snacks, String food, String water){
+    RegularTicket(int num, int passengerCount){
+        super(num, "Regular", "Delhi", "Mumbai", 0500, 1, true);
+    }
 
-        super("PNR", "Regular", "Delhi", "Mumbai", 0500, 1, true);
+    public void getService(){
+        System.out.println("Special Service: " + this.specialService);
+    }
 
-        System.out.println("Special Services Included: ");
-        if (snacks == "true"){
-            System.out.println("Snacks Included");
-        }
-        if (food == "true"){
-            System.out.println("Food Included");
-        }
-        if (water == "true"){
-            System.out.println("Water Included");
-        }
+    public void setService(String service){
+        this.specialService = service;
     }
 };
 
+// Tourist Ticket Class inheriting Ticket Class
 class TouristTicket extends Ticket{
 
-    String hotelInfo;
-    String hotelName;
-    String hotelAddress;
+    private String hotelAddress;
 
-    String location1;
-    String location2;
-    String location3;
-    String location4;
-    String location5;
 
-    public TouristTicket(String ticketPNR, String ticketClass, String ticketPrice, String destination, int departure, int seatNum, boolean ticketStat) {
-        super(ticketPNR, ticketClass, ticketPrice, destination, departure, seatNum, ticketStat);
+    public TouristTicket(int num, int psx){
+        super(num, "Tourist", "2000", "Mumbai", 0122, 26, true);
     }
 
-    public static void TouristTicket(String hotelInfo, String hotelName, String hotelAddress, String location1, String location2, String location3, String location4, String location5){
-        System.out.println("Hotel Info: " + hotelInfo);
-        System.out.println("Hotel Name: " + hotelName);
-        System.out.println("Hotel Address: " + hotelAddress);
-        System.out.println("Locations to be visited: ");
-        System.out.println("Location 1: " + location1);
-        System.out.println("Location 2: " + location2);
-        System.out.println("Location 3: " + location3);
-        System.out.println("Location 4: " + location4);
-        System.out.println("Location 5: " + location5);
+    // Setting Hotel Address
+    public void setHotelAddress(String hotelAddress) {
+        this.hotelAddress = hotelAddress;
+    }
+
+    // Getting Hotel Address
+    public void getHotelAddress() {
+        System.out.println("Hotel Address: " + this.hotelAddress);
+    }
+
+    // Getting Tourist Ticket Info
+    public void getTouristTicketInfo(){
+        super.TicketDetails();
+    }
+
+    // Getting Tourist Locations
+    public void getTouristLocation(int pnr){
+        System.out.println("Tourist Locations are: " + Location.get(pnr));
+    }
+
+    Flight flight = new Flight();
+
+    Map<Integer, String> Location = flight.getLocation();
+
+    public void setTouristLocation(String location){
+        Location.put(super.getPnr(), location);
     }
 };
 
@@ -235,8 +226,6 @@ public class Main {
                 while (n>0){
                     // Construct Passenger
                     Passenger new_pass = new Passenger();
-                    Contact new_cont = new Contact();
-
                     // Passenger Details
                     System.out.println("Enter your name: ");
                     String name = ns.next();
@@ -245,12 +234,10 @@ public class Main {
                     System.out.println("Enter your email: ");
                     String email = ns.next();
 
-                    new_cont.setEmail(email);
-                    new_cont.setPhone(phone);
-                    new_cont.setName(name);
+                    new_pass.getContact(name, phone, email);
+
 
                     // Address Details
-                    Address new_add = new Address();
 
                     System.out.println("Fill in your address details: ");
                     System.out.println("Enter your street: ");
